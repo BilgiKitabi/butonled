7a66b88a1dfb94e954895948bd89351ae6889bba
BilgiKitabi <guney.bilgikitabi@gmail.com>
2016-05-15 17:46:17 +0300

    Create ButonLed App

---
+++ butonled.ino
@@ -0,0 +1,27 @@
+const int buton = 9;
+const int led = 8;
+
+
+void setup () {
+pinMode(led, OUTPUT);
+pinMode(buton, INPUT);
+}
+
+void loop () {
+int butondeger = digitalRead(buton)
+if (butondeger = 1) 
+{
+ digitalWrite(led, HIGH),
+ }
+if (butondeger = 0)
+ { 
+ digitalWrite(led, LOW);
+ }
+}
+          -------
+          -
+          -
+          -
+          -
+          -
+          --------
